43fe66dc891f54df9b66315330dd9c3029015577
xieyajie <xyjxyf@163.com>
2014-11-07 11:59:33 +0800

    no message

--- ChatDemo-UI2.0/ChatDemo-UI2.0/Class/Chat/ChatView/ChatViewController.m
+++ ChatDemo-UI2.0/ChatDemo-UI2.0/Class/Chat/ChatView/ChatViewController.m
@@ -757,20 +757,23 @@
 
 - (void)moreViewAudioCallAction:(DXChatBarMoreView *)moreView
 {
-    __weak typeof(self) weakSelf = self;
-    if([[AVAudioSession sharedInstance] respondsToSelector:@selector(requestRecordPermission:)])
-    {
-        //requestRecordPermission
-        [[AVAudioSession sharedInstance] requestRecordPermission:^(BOOL granted) {
-            NSLog(@"granted = %d",granted);
-            if(granted)
-            {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    [[NSNotificationCenter defaultCenter] postNotificationName:@"callOutWithChatter" object:weakSelf.chatter];
-                });
-            }
-        }];
-    }
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"callOutWithChatter" object:self.chatter];
+    
+//    __weak typeof(self) weakSelf = self;
+//    if([[AVAudioSession sharedInstance] respondsToSelector:@selector(requestRecordPermission:)])
+//    {
+//        //requestRecordPermission
+//        [[AVAudioSession sharedInstance] requestRecordPermission:^(BOOL granted) {
+//            NSLog(@"granted = %d",granted);
+//            if(granted)
+//            {
+//                dispatch_async(dispatch_get_main_queue(), ^{
+//                    [[NSNotificationCenter defaultCenter] postNotificationName:@"callOutWithChatter" object:weakSelf.chatter];
+//                });
+//            }
+//        }];
+//    }
 }
 
 #pragma mark - LocationViewDelegate
