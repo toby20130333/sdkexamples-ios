ad966698555fe4532543c9996f62d02b498aec60
xieyajie <xyjxyf@163.com>
2014-11-11 12:14:19 +0800

    new project UI3.0

---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Helper/SendMessageHelper.h
@@ -0,0 +1,13 @@
+//
+//  SendMessageHelper.h
+//  ChatDemo-UI3.0
+//
+//  Created by dhcdht on 14-11-11.
+//  Copyright (c) 2014年 easemob.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SendMessageHelper : NSObject
+
+@end
